0f786ec91473aa9caecd3fbc6d950e3b9cb0bde6
Rabbi Iqbal <rabbi425@gmail.com>
2022-08-12 02:05:27 -0400

    update: font size reduced to display on mobile device

--- src/components/organisms/UserInfoForm.js
+++ src/components/organisms/UserInfoForm.js
@@ -1,4 +1,4 @@
-import { Button, Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
